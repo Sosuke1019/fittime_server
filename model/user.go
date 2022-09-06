1293975fd120327ade0250e071e2b860529d88bd
Irori235 <hiro030313@gmail.com>
2022-09-06 19:21:22 +0900

    :wrench: fix docker-compose

--- model/user.go
+++ model/user.go
@@ -4,7 +4,7 @@ import "github.com/google/uuid"
 
 // User型を定義する
 type User struct {
-	ID       uuid.UUID
+	ID       uuid.UUID `gorm:"primary_key"`
 	name     string
 	mail     string
 	password string
